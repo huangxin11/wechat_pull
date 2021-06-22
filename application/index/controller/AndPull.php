8175b5c2871a5844c5cc1ee1de5750888ea2b4ba
huangxin <973718378@qq.com>
2021-06-22 17:55:13 +0800

    测试

--- application/index/controller/AndPull.php
+++ application/index/controller/AndPull.php
@@ -19,6 +19,7 @@ class AndPull extends Controller
         logResult('进入拉取状态!');
         $model = new AndPullModel();
         $accessToken = $model->getAccessToken($this->appid, $this->secret);
+        logResult($accessToken);
         if (array_key_exists('errcode', $accessToken)) {
             $str = '状态码:' . $accessToken['errcode'] . '++报错信息:' . $accessToken['errmsg'];
             switch ($accessToken['errcode']) {
